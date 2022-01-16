7863583ba675ddc7560541d03c67ae198114ee2e
horhoj <horhoj@yandex.ru>
2022-01-16 15:48:43 +0500

    fix: test

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const DEFAULT_WAYPOINTS_DATA: WaypointsData = {
   1: { title: 'Точка 1', location: [55.65, 37.17] },
   2: { title: 'Точка 2', location: [55.55, 37.27] },
   3: { title: 'Точка 3', location: [55.75, 37.37] },
-  4: { title: 'Точка 4', location: [55.7, 37.79] },
+  4: { title: 'Точка 4', location: [55.76, 37.79] },
 };
 
 export const DEFAULT_WAYPOINT_ID_LIST: WaypointIdList = [1, 2, 3, 4];
